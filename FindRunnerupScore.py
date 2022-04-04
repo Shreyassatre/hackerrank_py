0e0624dc78632c5c841f4f9cb528737fee718a4e
Shreyassatre <61698348+Shreyassatre@users.noreply.github.com>
2022-04-04 10:04:05 +0530

    Create FindRunnerupScore.py

---
+++ FindRunnerupScore.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    arr.sort()
+    
+    for i in range(-1, -n, -1):
+        if(arr[i] == arr [i-1]):
+            continue
+        else:
+            print(arr[i-1])
+            break
