3c0204b4a7981d1be1815604c28855afde8e8910
Shreyassatre <satreshreyas@gmail.com>
2022-04-04 06:02:47 +0530

    Hacker Rank codes

---
+++ sortedlist.py
@@ -0,0 +1,29 @@
+if __name__ == '__main__':
+    students = []
+    newlis = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        
+        details = [name, score]
+        students.append(details)
+    
+    students.sort(key=lambda x:x[1])
+
+    for i in range(1,len(students)):
+        if(students[i-1][1] == students[i][1]):
+            continue
+        newlis.append(students[i])
+        break
+    
+    for j in range(i+1, len(students)):
+        if(students[j-1][1] == students[j][1]):
+            newlis.append(students[j])
+        else:
+            break
+    
+    newlis.sort(key = lambda x:x[0])
+    
+    for k in range(0, len(newlis)):
+        print(newlis[k][0])
+        
