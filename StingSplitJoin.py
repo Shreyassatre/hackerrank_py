526a6dafa4e68c80bb4dd4c5c42c5130c1eafd53
Shreyassatre <61698348+Shreyassatre@users.noreply.github.com>
2022-04-06 14:14:16 +0530

    Update StingSplitJoin.py

--- StingSplitJoin.py
+++ StingSplitJoin.py
@@ -1,6 +1,22 @@
+#Program to replace spaces with '-' using built in string methods
 def split_and_join(line):
     line_lis = line.split()
     return "-".join(line_lis)
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
+
+#Pogram to replace spaces with '-' without using built in sting methods
+def split_and_join(line):
+    newstr=''
+    for char in line:
+        if(char == " "):
+            char = '-'
+        newstr += char
+    
+    return newstr
 
 if __name__ == '__main__':
     line = input()
