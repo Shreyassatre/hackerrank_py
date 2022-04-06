7d6e37c7742c353e4b7c0cf6019157348aba86fd
Shreyassatre <61698348+Shreyassatre@users.noreply.github.com>
2022-04-06 14:22:17 +0530

    Update listComprehensions.py

--- listComprehensions.py
+++ listComprehensions.py
@@ -1,3 +1,4 @@
+#Print a list of all possible coordinates given by(i,j,k)on a 3D grid where the sum i+j+k of  is not equal to n.
 if __name__ == '__main__':
     x = int(input())
     y = int(input())
