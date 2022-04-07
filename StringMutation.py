a07bf9b1614dd60686768dcba5e5749869c9885e
Shreyassatre <61698348+Shreyassatre@users.noreply.github.com>
2022-04-07 09:22:29 +0530

    Update StringMutation.py

--- StringMutation.py
+++ StringMutation.py
@@ -1,11 +1,19 @@
 #You are given an immutable string, and you want to make changes to it.
 def mutate_string(string, position, character):
-    string_lis = list(string)
-    
+    string_lis = list(string)    
     string_lis[position] = character
-    
     string = ''.join(string_lis)
-    
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+
+#Another method
+def mutate_string(string, position, character):
+    string = string[:position]+character+string[position+1:]
     return string
 
 if __name__ == '__main__':
