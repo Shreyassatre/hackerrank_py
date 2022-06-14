20564cc8564397054a371b45660027470117cff3
Shreyassatre <61698348+Shreyassatre@users.noreply.github.com>
2022-06-14 07:35:14 +0530

    Update StringAlligment.py

--- StringAlligment.py
+++ StringAlligment.py
@@ -1,23 +1,24 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-n = int(input())
-var1 = n//2+1
+if __name__ == '__main__':
+    n = int(input())
+    var1 = n//2+1
 
-for i in range(1, n*(n//2), 2):  
-    print(('H'*i).center(n*(n//2),' '), end='\n')
-    
-for i in range(0, n+1):
-    print(('H'*n).rjust(n+n//2,' '), end=' '*n*var1) 
-    print(('H'*n).ljust(n+n//2,' '), end='\n') 
-       
-for i in range(0, n//2+1):
-    print(('H'*n*n).rjust(n*n+n//2, ' '), end='\n')
-    
-for i in range(0, n+1):
-    print(('H'*n).rjust(n+n//2,' '), end=' '*n*var1) 
-    print(('H'*n).ljust(n+n//2,' '), end='\n') 
-    
-for i in range(n*(n//2)-1, 0, -2):  
-    print(('H'*i).center(n*n*2,' '), end='\n')
+    for i in range(1, n*(n//2), 2):  
+        print(('H'*i).center(n*(n//2),' '), end='\n')
+
+    for i in range(0, n+1):
+        print(('H'*n).rjust(n+n//2,' '), end=' '*n*var1) 
+        print(('H'*n).ljust(n+n//2,' '), end='\n') 
+
+    for i in range(0, n//2+1):
+        print(('H'*n*n).rjust(n*n+n//2, ' '), end='\n')
+
+    for i in range(0, n+1):
+        print(('H'*n).rjust(n+n//2,' '), end=' '*n*var1) 
+        print(('H'*n).ljust(n+n//2,' '), end='\n') 
+
+    for i in range(n*(n//2)-1, 0, -2):  
+        print(('H'*i).center(n*n*2,' '), end='\n')
 
 # OUTPUT
 
